702aaeafcff3c73b177861d4488be27bc83819dc
Marvin Sielenkemper <1593106+sielenk@users.noreply.github.com>
2000-11-11 15:25:49 +0000

    - modified to use the new ContinuationStack - made evaluate public

--- MScheme/machine/Machine.java
+++ MScheme/machine/Machine.java
@@ -7,20 +7,16 @@ import MScheme.exceptions.SException;
 
 public class Machine
 {
-    static SExpr evaluate(
+    public static SExpr evaluate(
         Environment environment,
-        SExpr sexpr
+        SExpr       sexpr
     ) throws SException {
-        ContinuationStack stack = new ContinuationStack();
+        ContinuationStack stack = new ContinuationStack(environment);
 
-        stack.push(EvalFunc.INSTANCE, environment);
+        stack.push(EvalFunc.INSTANCE);
 
-        {
-            Continuation cc;
-
-            while ((cc = stack.getTop()) != null) {
-                sexpr = cc.invoke(stack, sexpr);
-            }
+        while (!stack.isEmpty()) {
+            sexpr = stack.getTop().invoke(stack, sexpr);
         }
 
         return sexpr;
