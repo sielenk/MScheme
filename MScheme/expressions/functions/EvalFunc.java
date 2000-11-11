832a57205c6bde3bed2c3a65629060be03f0d4a4
Marvin Sielenkemper <1593106+sielenk@users.noreply.github.com>
2000-11-11 11:15:32 +0000

    changed SException to more appropriate ones

--- MScheme/expressions/functions/EvalFunc.java
+++ MScheme/expressions/functions/EvalFunc.java
@@ -8,43 +8,46 @@ import MScheme.expressions.SVector;
 import MScheme.expressions.SValues;
 
 import MScheme.exceptions.SException;
+import MScheme.exceptions.SExpectedListException;
+import MScheme.exceptions.SCantEvaluateException;
+
 import MScheme.machine.ContinuationStack;
 import MScheme.environment.Environment;
 
 public class EvalFunc extends Function
 {
     public final static Function INSTANCE = new EvalFunc();
-    
+
     private EvalFunc() { }
-    
+
 
     public SExpr call(
-	ContinuationStack stack,
-	Environment environment,
-	SExpr sexpr
+        ContinuationStack stack,
+        Environment environment,
+        SExpr sexpr
     ) throws SException {
-	if (sexpr instanceof SSymbol) {
-	    return environment.lookup((SSymbol)sexpr);
-	} else if (sexpr instanceof SPair) {
-	    SPair pair = (SPair)sexpr;
+        if (sexpr instanceof SSymbol) {
+            return environment.lookup((SSymbol)sexpr);
+            // throws SSymbolNotFoundException
+        } else if (sexpr instanceof SPair) {
+            SPair pair = (SPair)sexpr;
 
-	    try {
-		stack.push(
-		    new ExpectFunctionFunc(
-			((SList)pair.getCdr()).toValues()
-		    )
-		);
+            try {
+                stack.push(
+                    new ExpectFunctionFunc(
+                        ((SList)pair.getCdr()).toValues()
+                    )
+                );
 
-		stack.push(INSTANCE);
-		return pair.getCar();
-	    } catch (ClassCastException e) {
-		throw new SException(pair.getCdr());
-	    }
-	} else if (sexpr instanceof SVector) {
-	    throw new SException(sexpr);
-	} else {
-	    return sexpr;
-	}
+                stack.push(INSTANCE);
+                return pair.getCar();
+            } catch (ClassCastException e) {
+                throw new SExpectedListException(pair.getCdr());
+            }
+        } else if (sexpr instanceof SVector) {
+            throw new SCantEvaluateException(sexpr);
+        } else {
+            return sexpr;
+        }
     }
 }
-
