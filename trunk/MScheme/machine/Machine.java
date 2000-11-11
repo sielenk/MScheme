dfc610f99ba965042ec4f1b09ede2fbcf694c8a3
sielenk <sielenk@14e62dca-297b-4487-9397-64a3d55ac3ab>
2000-11-11 11:35:37 +0000

    changed the run(...) method to a static evaluate(...) method

--- trunk/MScheme/machine/Machine.java
+++ trunk/MScheme/machine/Machine.java
@@ -1,23 +1,28 @@
 package MScheme.machine;
 
+import MScheme.environment.Environment;
 import MScheme.expressions.SExpr;
+import MScheme.expressions.functions.EvalFunc;
+import MScheme.exceptions.SException;
 
 public class Machine
 {
-    private ContinuationStack _stack = new ContinuationStack();
-    
-	SExpr run(SExpr sexpr)
-	{
-	    try {
-    	    Continuation cc;
-	    
-    	    while ((cc = _stack.getTop()) != null) {
-    	        sexpr = cc.invoke(_stack, sexpr);
-		    }
-		}
-		catch (Exception e) { }
-		
-		return sexpr;
-	}
-}
+    static SExpr evaluate(
+        Environment environment,
+        SExpr sexpr
+    ) throws SException {
+        ContinuationStack stack = new ContinuationStack();
 
+        stack.push(EvalFunc.INSTANCE, environment);
+
+        {
+            Continuation cc;
+
+            while ((cc = stack.getTop()) != null) {
+                sexpr = cc.invoke(stack, sexpr);
+            }
+        }
+
+        return sexpr;
+    }
+}
