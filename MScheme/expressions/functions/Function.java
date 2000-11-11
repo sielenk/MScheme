a6d6c8377ec1a2d35b33c96ac33e52ce03e60e01
Marvin Sielenkemper <1593106+sielenk@users.noreply.github.com>
2000-11-11 17:41:33 +0000

    - added arity attributes and check - made 'call' method final (it was abstract - now it does the arity check) - added new abstact '_call' method - added '_checkArity' method

--- MScheme/expressions/functions/Function.java
+++ MScheme/expressions/functions/Function.java
@@ -1,16 +1,62 @@
 package MScheme.expressions.functions;
 
 import MScheme.expressions.SExpr;
+import MScheme.machine.Values;
+
 import MScheme.exceptions.SException;
+import MScheme.exceptions.SWrongArgumentCountException;
+
 import MScheme.machine.ContinuationStack;
 import MScheme.environment.Environment;
 
+
 public abstract class Function extends SExpr
 {
+    private int _minArity;
+    private int _maxArity;
 
-    abstract public SExpr call(
+
+    protected Function(
+        int minArity,
+        int maxArity
+    ) {
+        _minArity = minArity;
+        _maxArity = maxArity;
+    }
+
+
+    abstract protected Values _call(
         ContinuationStack stack,
-        Environment environment,
-        SExpr sexpr
+        Environment       environment,
+        Values            aruments
     ) throws SException;
+
+
+    final public Values call(
+        ContinuationStack stack,
+        Environment       environment,
+        Values            arguments
+    ) throws SException {
+        _checkArity(arguments);
+
+        return _call(stack, environment, arguments);
+    }
+
+
+    private void _checkArity(
+        Values arguments
+    ) throws SException {
+        int len = arguments.getLength();
+
+        if (
+            (len < _minArity)
+            || ((_maxArity != -1) && (_maxArity < len))
+        ) {
+            throw new SWrongArgumentCountException(
+                arguments.toList(),
+                _minArity,
+                _maxArity
+            );
+        }
+    }
 }
