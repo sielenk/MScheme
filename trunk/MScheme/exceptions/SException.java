3be9552e5041d466ce68dd70f8b0647a98a1361f
sielenk <sielenk@14e62dca-297b-4487-9397-64a3d55ac3ab>
2000-11-11 11:19:11 +0000

    made SException abstract

--- trunk/MScheme/exceptions/SException.java
+++ trunk/MScheme/exceptions/SException.java
@@ -2,19 +2,20 @@ package MScheme.exceptions;
 
 import MScheme.expressions.SExpr;
 
-public class SException extends Exception 
+public abstract class SException extends Exception
 {
     private SExpr _sexpr;
-    
-    SException(SExpr sexpr)
-    {
+
+    SException(
+        SExpr sexpr
+    ) {
         super();
         _sexpr = sexpr;
-	}
-	
-	SExpr getSExpr()
-	{
-    	return _sexpr;
-	}
+    }
+
+    SExpr getSExpr()
+    {
+        return _sexpr;
+    }
 }
 
