20bd886ef825e46f1fcd7537e555aaf77d16bc7b
Marvin Sielenkemper <1593106+sielenk@users.noreply.github.com>
2000-11-11 15:26:36 +0000

    - added some test code

--- MScheme/Main.java
+++ MScheme/Main.java
@@ -1,9 +1,31 @@
 package MScheme;
 
+import MScheme.expressions.*;
+import MScheme.exceptions.SException;
+import MScheme.environment.Environment;
+import MScheme.machine.Machine;
 
 public class Main
 {
     public static void main(String argv[])
     {
+        Environment env  = new Environment();
+        SExpr       expr = new SPair(
+            SEmpty.INSTANCE,
+            SEmpty.INSTANCE
+        );
+
+        try {
+            System.out.println(
+                Machine.evaluate(env, expr)
+            );
+        }
+        catch (SException e) {
+            System.err.println(
+                e.getSExpr()
+                + " caused an "
+                + e.getClass()
+            );
+        }
     }
 }
