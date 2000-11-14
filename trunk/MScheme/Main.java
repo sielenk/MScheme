26c6419fa636575dec3f3165eae2189b06fd7482
sielenk <sielenk@14e62dca-297b-4487-9397-64a3d55ac3ab>
2000-11-14 16:24:30 +0000

    a first test case

--- trunk/MScheme/Main.java
+++ trunk/MScheme/Main.java
@@ -1,6 +1,7 @@
 package MScheme;
 
 import MScheme.expressions.*;
+import MScheme.expressions.functions.*;
 import MScheme.exceptions.SException;
 import MScheme.environment.Environment;
 import MScheme.environment.EnvironmentFactory;
@@ -11,10 +12,35 @@ public class Main
     public static void main(String argv[])
     {
         Environment env  = EnvironmentFactory.getEmpty();
-        SExpr       expr = new SPair(
-            SEmpty.INSTANCE,
-            SEmpty.INSTANCE
-        );
+        SExpr       expr =
+            new SListFactory()
+            .append(SyntaxFunc.BEGIN_FUNC)
+            .append(
+                new SListFactory()
+                .append(SyntaxFunc.DEFINE_FUNC)
+                .append(new SSymbol("f"))
+                .append(
+                    new SListFactory()
+                    .append(SyntaxFunc.LAMBDA_FUNC)
+                    .append(
+                            new SListFactory()
+                            .append(new SSymbol("x"))
+                            .append(new SSymbol("y"))
+                            .getList()
+                    )
+                    .append(new SSymbol("x"))
+                    .getList()
+                )
+                .getList()
+            )
+            .append(
+                new SListFactory()
+                .append(new SSymbol("f"))
+                .append(SBool.FALSE)
+                .append(SBool.TRUE)
+                .getList()
+            )
+            .getList();
 
         try {
             System.out.println(
@@ -23,7 +49,8 @@ public class Main
         }
         catch (SException e) {
             System.err.println(
-                e.getSExpr()
+                "\n"
+                + e.getSExpr()
                 + " caused an "
                 + e.getClass().getName()
             );
