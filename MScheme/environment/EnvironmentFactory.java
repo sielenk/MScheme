aa9b5b94a1db14216880e829a9ac55e43b45c0fe
Marvin Sielenkemper <1593106+sielenk@users.noreply.github.com>
2000-11-14 16:09:03 +0000

    - moved Environment fill(...) into EnvironmentStub[Impl]

--- MScheme/environment/EnvironmentFactory.java
+++ MScheme/environment/EnvironmentFactory.java
@@ -1,33 +1,13 @@
 package MScheme.environment;
 
 
-import MScheme.expressions.SExpr;
-
-import MScheme.machine.Values;
-
-
 public class EnvironmentFactory
 {
     // ***********************************************************************
 
     public static Environment getEmpty()
     {
-        return new EnvironmentImpl(null);
-    }
-
-
-    public static Environment fill(
-        EnvironmentStub stub,
-        int             minArity,
-        boolean         allowMore,
-        Values          values
-    ) {
-        return new EnvironmentImpl(
-            (EnvironmentStubImpl)stub,
-            minArity,
-            allowMore,
-            values
-        );
+        return new EnvironmentImpl();
     }
 
     // ***********************************************************************
