88ba2704c73d4f08ba35e8dac179db538475aeb9
Marvin Sielenkemper <1593106+sielenk@users.noreply.github.com>
2000-11-19 19:21:37 +0000

    - added an int attribute - added a constructor, add and mul methods

--- MScheme/expressions/SNumber.java
+++ MScheme/expressions/SNumber.java
@@ -2,10 +2,25 @@ package MScheme.expressions;
 
 public class SNumber extends SExpr
 {
+    private int _value;
+
+
+    public SNumber(int value)
+    {
+        _value = value;
+    }
+
+
+    public SNumber add(SNumber other)
+    { return new SNumber(_value + other._value); }
+
+
+    public SNumber mul(SNumber other)
+    { return new SNumber(_value * other._value); }
 
 
     protected String defaultString()
     {
-        return "";
+        return "" + _value;
     }
 }
