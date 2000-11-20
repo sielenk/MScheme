c057df3fc72e6d20922f6af73451ea6cc8de84a6
Marvin Sielenkemper <1593106+sielenk@users.noreply.github.com>
2000-11-20 18:51:39 +0000

    - added the SEmpty class - added final static SList EMPTY attribute - added static cons method

--- MScheme/expressions/SList.java
+++ MScheme/expressions/SList.java
@@ -5,8 +5,32 @@ import MScheme.machine.Values;
 import MScheme.exceptions.SImproperListException;
 
 
+class SEmpty extends SList
+{
+    SEmpty() { }
+
+    public Values toValues()
+    { return Values.EMPTY; }
+
+
+    protected String defaultString()
+    {
+        return "()";
+    }
+}
+
+
 abstract public class SList extends SExpr
 {
+    public final static SList EMPTY = new SEmpty();
+
+
     abstract public Values toValues()
         throws SImproperListException;
+
+
+    public static SList cons(SExpr car, SExpr cdr)
+    {
+        return new SPair(car, cdr);
+    }
 }
