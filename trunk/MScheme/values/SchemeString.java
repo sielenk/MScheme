439c66f6b6a06df1d198813c6a7abce370c85aea
sielenk <sielenk@14e62dca-297b-4487-9397-64a3d55ac3ab>
2001-06-02 16:41:49 +0000

    added: immutable literal handling added: new method 'public SchemeString toScmString()'

--- trunk/MScheme/values/SchemeString.java
+++ trunk/MScheme/values/SchemeString.java
@@ -3,14 +3,21 @@ package MScheme.values;
 import java.io.Writer;
 import java.io.IOException;
 
+import MScheme.exceptions.ImmutableException;
+
 
 public final class SchemeString
     extends SelfEvaluatingValue
 {
-    private final String _string;
+    private String  _string;
+    private boolean _isLiteral = false;
+
     
     public SchemeString(String value)
     { _string = value; }
+
+    public void setLiteral()
+    { _isLiteral = true; }
      
     public String getString()
     { return _string; }
@@ -20,7 +27,21 @@ public final class SchemeString
     
     public boolean isString()
     { return true; }
-    
+
+    public SchemeString toScmString()
+    { return this; }
+
+
+    // accessors
+
+    public void set(int index, char c)
+        throws ImmutableException
+    {
+        if (_isLiteral) {
+            throw new ImmutableException(this);
+        }
+    }
+
     public boolean equal(Value other)
     {
         try {
