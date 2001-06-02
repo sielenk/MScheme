ab5a11f79ca1e516f2a7478f8440820ccbb35a9f
sielenk <sielenk@14e62dca-297b-4487-9397-64a3d55ac3ab>
2001-06-02 16:50:21 +0000

    created due to the new type casts in Value

---
+++ trunk/MScheme/exceptions/CharExpectedException.java
@@ -0,0 +1,12 @@
+package MScheme.exceptions;
+
+import MScheme.values.Value;
+
+
+public class CharExpectedException
+    extends ExpectedException
+{
+    public CharExpectedException(Value cause)
+    { super(cause); }
+}
+
