40615050fce000f69bcfe0d2d0022a8f51e8fc0f
sielenk <sielenk@14e62dca-297b-4487-9397-64a3d55ac3ab>
2001-06-03 12:48:50 +0000

    changed: now uses the default constructor

--- trunk/MScheme/Main.java
+++ trunk/MScheme/Main.java
@@ -10,44 +10,13 @@ public class Main
     public final static void main(String[] argv)
         throws Exception
     {
-        InputPort  in  = ValueFactory.createInputPort();
-        OutputPort out = ValueFactory.createOutputPort();
-
-        DynamicEnvironment environment
-            = DynamicEnvironment.getEmpty();
-    
-        StaticEnvironment staticBindings
-            = environment.getStatic();
-        
-        staticBindings.defineSyntax(
-            ValueFactory.createSymbol("quote"),
-            SyntaxFactory.getQuoteToken()
-        );
-        staticBindings.defineSyntax(
-            ValueFactory.createSymbol("if"),
-            SyntaxFactory.getIfToken()
-        );
-        staticBindings.defineSyntax(
-            ValueFactory.createSymbol("begin"),
-            SyntaxFactory.getBeginToken()
-        );
-        staticBindings.defineSyntax(
-            ValueFactory.createSymbol("lambda"),
-            SyntaxFactory.getLambdaToken()
-        );
-        staticBindings.defineSyntax(
-            ValueFactory.createSymbol("define"),
-            SyntaxFactory.getDefineToken()
-        );
-        staticBindings.defineSyntax(
-            ValueFactory.createSymbol("set!"),
-            SyntaxFactory.getSetToken()
-        );
-
-        Machine machine = new Machine(environment);
+        InputPort  in      = ValueFactory.createInputPort();
+        OutputPort out     = ValueFactory.createOutputPort();
+        Machine    machine = new Machine();
         
         out.write(machine.evaluate(in.read()));
         
         out.writeChar('\n');
     }
 }
+
