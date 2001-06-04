744a64905d98ebb221af3ae1fdfc28541b41c2c1
sielenk <sielenk@14e62dca-297b-4487-9397-64a3d55ac3ab>
2001-06-04 17:07:42 +0000

    cleanups and removed the abstract checkedCall

--- trunk/MScheme/functions/TernaryFunction.java
+++ trunk/MScheme/functions/TernaryFunction.java
@@ -1,23 +1,9 @@
 package MScheme.functions;
 
-import MScheme.machine.Machine;
-import MScheme.code.Code;
-import MScheme.values.Value;
-
-import MScheme.exceptions.SchemeException;
-
-
 abstract public class TernaryFunction
     extends CheckedFunction
 {
     protected TernaryFunction()
     { super(3); }
-    
-    abstract protected Code checkedCall(
-        Machine machine,
-        Value   first,
-        Value   second,
-        Value   third
-    ) throws SchemeException;
 }
 
