0d9bc32a324ab57fa82a83b7310ca828176ea865
Marvin Sielenkemper <1593106+sielenk@users.noreply.github.com>
2001-06-04 17:12:10 +0000

    base class for assq, assv and assoc

---
+++ MScheme/functions/AssocBase.java
@@ -0,0 +1,36 @@
+package MScheme.functions;
+
+import MScheme.values.ValueFactory;
+import MScheme.values.Value;
+import MScheme.values.List;
+import MScheme.values.Pair;
+
+import MScheme.exceptions.ListExpectedException;
+
+
+abstract class AssocBase
+    extends BinaryFunction
+{
+    protected abstract boolean equal(Value fst, Value snd);
+
+    protected final Value checkedCall(
+        Value key,
+        Value values
+    ) throws ListExpectedException
+    {
+        List tail = values.toList();
+
+        while (!tail.isEmpty()) {
+            Pair pair = tail.getHead().toPair();
+
+            if (equal(key, pair.getHead())) {
+                return pair;
+            }
+
+            tail = tail.getTail();
+        }
+
+        return ValueFactory.createFalse();
+    }
+}
+
