bb80465a78d2b223e6d6af7ce76abad36531c7ec
sielenk <sielenk@14e62dca-297b-4487-9397-64a3d55ac3ab>
2001-06-04 17:12:31 +0000

    base class for memq, memv and member

---
+++ trunk/MScheme/functions/MemberBase.java
@@ -0,0 +1,33 @@
+package MScheme.functions;
+
+import MScheme.values.ValueFactory;
+import MScheme.values.Value;
+import MScheme.values.List;
+
+import MScheme.exceptions.ListExpectedException;
+
+
+abstract class MemberBase
+    extends BinaryFunction
+{
+    protected abstract boolean equal(Value fst, Value snd);
+
+    protected final Value checkedCall(
+        Value key,
+        Value values
+    ) throws ListExpectedException
+    {
+        List tail = values.toList();
+
+        while (!tail.isEmpty()) {
+            if (equal(key, tail.getHead())) {
+                return tail;
+            }
+
+            tail = tail.getTail();
+        }
+
+        return ValueFactory.createFalse();
+    }
+}
+
