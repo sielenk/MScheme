455d6878954f06413760f0e9594f29deec540211
sielenk <sielenk@14e62dca-297b-4487-9397-64a3d55ac3ab>
2001-06-04 17:14:56 +0000

    base class for unary predicates like pair?, number?, ...

---
+++ trunk/MScheme/functions/UnaryPredicate.java
@@ -0,0 +1,21 @@
+package MScheme.functions;
+
+import MScheme.values.ValueFactory;
+import MScheme.values.Value;
+
+import MScheme.exceptions.SchemeException;
+
+
+abstract class UnaryPredicate
+    extends UnaryFunction
+{
+    protected abstract boolean test(
+        Value fst
+    ) throws SchemeException;
+
+    protected final Value checkedCall(
+        Value fst
+    ) throws SchemeException
+    { return ValueFactory.createBool(test(fst)); }
+}
+
