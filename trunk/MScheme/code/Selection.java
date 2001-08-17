e0bc1e7d291afbb4bade7931144902b9382ff196
sielenk <sielenk@14e62dca-297b-4487-9397-64a3d55ac3ab>
2001-08-17 10:13:16 +0000

    these were the Compiled[...].java files

---
+++ trunk/MScheme/code/Selection.java
@@ -0,0 +1,41 @@
+package MScheme.code;
+
+import MScheme.values.Value;
+import MScheme.machine.Machine;
+import MScheme.machine.Continuation;
+
+
+final public class Selection
+    extends Code
+{
+    final private Code _flag;
+    final private Code _onTrue;
+    final private Code _onFalse;
+
+    public Selection(
+        Code flag,
+        Code onTrue,
+        Code onFalse
+    )
+    {
+        _flag    = flag;
+        _onTrue  = onTrue;
+        _onFalse = onFalse;
+    }
+
+    class SelectContinuation
+        extends Continuation
+    {
+        SelectContinuation(Machine machine)
+        { super(machine); }
+
+        protected Code execute(Machine machine, Value evaluationResult)
+        { return evaluationResult.isTrue() ? _onTrue : _onFalse; }
+    }
+    
+    public Code executionStep(Machine machine)
+    {
+        new SelectContinuation(machine);
+        return _flag;
+    }
+}
