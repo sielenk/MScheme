9311a10f1b9236ddd0768fcd158ae5c3a29b3d0e
Marvin Sielenkemper <1593106+sielenk@users.noreply.github.com>
2001-08-20 10:15:08 +0000

    renamed class State -> Register moved the AbortContinuation back into Machine.java

--- MScheme/machine/Registers.java
+++ MScheme/machine/Registers.java
@@ -1,45 +1,19 @@
 package MScheme.machine;
 
-import MScheme.code.Code;
-import MScheme.values.Value;
 import MScheme.environment.Environment;
 import MScheme.functions.UnaryFunction;
 
 
-class AbortContinuation
-    extends Continuation
-{
-    private Value _result;
-
-    AbortContinuation(State state)
-    { super(state); _result = null; }
-
-    Value getResult()
-    { return _result; }
-
-    protected Code execute(State state, Value evaluationResult)
-    { _result = evaluationResult; return null; }
-}
-
-
-public class State
+public class Registers
 {
     private Continuation _continuation;
     private Environment  _environment;
-    final private AbortContinuation _abort;
 
-    State(Environment environment)
+    Registers(Environment environment)
     {
         _continuation = null;
         _environment  = environment;
-        _abort        = new AbortContinuation(this);
     }
-
-    Value getResult()
-    { return _abort.getResult(); }
-    
-    boolean isHalt()
-    { return _abort.getResult() != null; }
 
     void setContinuation(Continuation newContinuation)
     { _continuation = newContinuation; }
