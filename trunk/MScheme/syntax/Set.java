b7b6cc79fed87eff7b9582dcf12b101b6cb38d7b
sielenk <sielenk@14e62dca-297b-4487-9397-64a3d55ac3ab>
2001-09-04 09:19:30 +0000

    merged class Assign into class Set and removed class Assign reworked class Define to use class Set

--- trunk/MScheme/syntax/Set.java
+++ trunk/MScheme/syntax/Set.java
@@ -1,24 +1,51 @@
 package MScheme.syntax;
 
+import MScheme.util.Arity;
+
 import MScheme.Code;
+import MScheme.Value;
 import MScheme.Syntax;
 
+import MScheme.code.Assignment;
 import MScheme.environment.StaticEnvironment;
 import MScheme.environment.Reference;
 import MScheme.values.List;
 import MScheme.values.Symbol;
 
 import MScheme.exceptions.CompileError;
+import MScheme.exceptions.TypeError;
 
 
 final class Set
-    extends Assign
+    extends Syntax
 {
     final static Syntax INSTANCE = new Set();
     
-    protected Reference getReference(
+    private Set()
+    { super(Arity.exactly(2)); }
+
+    protected Code checkedTranslate(
         StaticEnvironment syntax,
-        Symbol            symbol
-    ) throws CompileError
-    { return symbol.getReference(syntax); }
+        List              arguments
+    ) throws CompileError, TypeError
+    {
+        Symbol symbol = arguments.getHead().toSymbol();
+        Value  value  = arguments.getTail().getHead();
+
+        return translate(
+            symbol.getReference(syntax),
+            value .getCode     (syntax)
+        );
+    }
+
+    static Code translate(
+        Reference reference,
+        Code      code
+    )
+    {
+        return new Assignment(
+            reference,
+            code
+        );
+    }
 }
