3ce7d83aabd051853ea06429c42a52e441eb9be2
Marvin Sielenkemper <1593106+sielenk@users.noreply.github.com>
2001-09-04 12:49:56 +0000

    removed unneeded spaces

--- MScheme/util/Arity.java
+++ MScheme/util/Arity.java
@@ -5,33 +5,33 @@ public class Arity
 {
     private final int _minArity;
     private final int _maxArity;
-    
+
     private Arity(int minArity, int maxArity)
     {
         _minArity = minArity;
         _maxArity = maxArity;
     }
-    
+
 
     public static Arity exactly(int arity)
     { return new Arity(arity, arity); }
-    
+
     public static Arity atLeast(int arity)
     { return new Arity(arity, -1); }
-    
+
     public static Arity inRange(int lo, int hi)
     { return new Arity(lo, hi); }
-    
-    
+
+
     public int getMin()
     { return _minArity; }
-    
+
     public int getMax()
     { return _maxArity; }
-    
+
     public boolean allowMore()
     { return (_maxArity == -1); }
-    
+
 
     public boolean isValid(int arity)
     {
@@ -41,19 +41,18 @@ public class Arity
 
         return (gotEnoughArguments && !gotTooManyArguments);
     }
-    
-    
+
+
     public String toString()
     {
         String result = "" + getMin();
-    
+
         if (allowMore()) {
             result += " or more";
         } else if (getMin() != getMax()) {
             result += " to " + getMax();
         }
-        
+
         return result;
     }
 }
-
