e87ad131b05c5b3419b19e8b5c372e47440edf1b
sielenk <sielenk@14e62dca-297b-4487-9397-64a3d55ac3ab>
2001-09-07 11:41:35 +0000

    added documentation

--- trunk/MScheme/Translator.java
+++ trunk/MScheme/Translator.java
@@ -9,8 +9,14 @@ import MScheme.exceptions.TypeError;
 import MScheme.exceptions.CompileError;
 
 
+/**
+ * This interface is used to compile lists. Due to the special
+ * nature of syntactic keywords in Scheme - they are not reserved -
+ * their bindings are stored in the static environment.
+ */
 public interface Translator
 {
+    /** The CVS id of the file containing this class. */
     String id
         = "$Id$";
 
