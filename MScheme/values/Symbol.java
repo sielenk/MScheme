470b56ac9473b547b9fa6b5bed775dd8af47244f
Marvin Sielenkemper <1593106+sielenk@users.noreply.github.com>
2001-09-24 15:57:43 +0000

    -- back to 2.10 -- removed Weak* stuff again added public boolean eq(Value other) again

--- MScheme/values/Symbol.java
+++ MScheme/values/Symbol.java
@@ -1,8 +1,5 @@
 package MScheme.values;
 
-import java.util.WeakHashMap;
-import java.lang.ref.WeakReference;
-
 import java.io.Writer;
 import java.io.IOException;
 
@@ -23,32 +20,13 @@ public final class Symbol
         = "$Id$";
 
 
-    private final static WeakHashMap _map = new WeakHashMap();
-
     private final String _javaString;
 
     private Symbol(String javaString)
-    { _javaString = javaString; }
+    { _javaString = javaString.intern(); }
 
     public static Symbol create(String javaString)
-    {
-        final WeakReference ref   = (WeakReference)_map.get(javaString);
-        Symbol              value = (ref != null) ? (Symbol)ref.get() : null;
-
-        // value might be null, even if ref isn't.
-        // This happens, if the wealky referenced value is already collected
-        // but it's map entry is still in place.
-
-        if (value == null) {
-            value = new Symbol(javaString);
-            //        v-  and  -^ have to be the same ...
-            _map.put(javaString, new WeakReference(value));
-            // ... to ensure the collection of the wealky referenced
-            // value before it's map entry
-        }
-
-        return value;
-    }
+    { return new Symbol(javaString); }
 
     public static Symbol create(ScmString schemeString)
     { return create(schemeString.getJavaString()); }
@@ -69,6 +47,19 @@ public final class Symbol
 
     public Symbol toSymbol()
     { return this; }
+
+
+    public boolean eq(Value other)
+    {
+        try {
+            Symbol otherSymbol = (Symbol)other;
+            
+            return getJavaString() == otherSymbol.getJavaString();
+        }
+        catch (ClassCastException e) { }
+
+        return false;
+    }
 
 
     public void write(Writer destination)
