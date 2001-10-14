aceb1e57071f4d16477581d72ced75fb4c690dd7
Marvin Sielenkemper <1593106+sielenk@users.noreply.github.com>
2001-10-14 13:09:24 +0000

    new class for subcontinuation support

---
+++ MScheme/functions/SpawnFunction.java
@@ -0,0 +1,36 @@
+package MScheme.functions;
+
+import MScheme.machine.Registers;
+
+import MScheme.Value;
+import MScheme.List;
+import MScheme.Code;
+
+import MScheme.values.ListFactory;
+
+import MScheme.exceptions.*;
+
+
+public final class SpawnFunction
+    extends UnaryFunction
+{
+    public final static String id
+        = "$Id$";
+
+
+    public final static SpawnFunction INSTANCE = new SpawnFunction();
+
+    private SpawnFunction()
+    { }
+
+    protected Code checkedCall(Registers state, Value argument)
+        throws SchemeException
+    {
+        return argument.toFunction().call(
+            state,
+            ListFactory.create(
+                state.getComputationController()
+            )
+        );
+    }
+}
