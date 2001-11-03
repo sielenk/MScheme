9754ad32331c8f050bc6e68c76ee0ef239c8ca3b
sielenk <sielenk@14e62dca-297b-4487-9397-64a3d55ac3ab>
2001-11-03 22:09:03 +0000

    changed some '(' and ')' to '[' and ']' added error reporting -- errors don't kill the interpreter anymore

--- trunk/MScheme/rep.scm
+++ trunk/MScheme/rep.scm
@@ -1,12 +1,12 @@
 ; -*- scheme -*-
 ; $Id$
 
-(let* ((user-env    (scheme-report-environment 5))
-       (user-eval   (lambda (expr)
-                      (eval expr user-env)))
-       (user-define (lambda (sym val)
-                      (user-eval (list 'define sym val))))
-       (user-load   (user-eval 'load)))
+(let* ([user-env    (scheme-report-environment 5)]
+       [user-eval   (lambda (expr)
+                      (eval expr user-env))]
+       [user-define (lambda (sym val)
+                      (user-eval (list 'define sym val)))]
+       [user-load   (user-eval 'load)])
 
   (define (REP-read depth prompt quit-thunk)
     (if prompt
@@ -30,10 +30,10 @@
     expr)
 
   (define (REP prompt)
-    (let internal-REP ((level 0))
+    (let internal-REP ([level 0])
       (call-with-current-continuation
         (lambda (quit)
-          (let loop ((result #f))
+          (let loop ([result #f])
             (loop
               (REP-print
                 (REP-eval
@@ -43,15 +43,38 @@
                     (lambda ()
                       (quit result)))))))))))
 
-  (dynamic-wind
-    (lambda ()
-      (display "Welcome to MScheme")
-      (newline)
-      (display "press Ctrl-D to quit")
-      (newline))
-    (lambda ()
-      (REP (lambda () (display "--> "))))
-    (lambda ()
-      (newline)
-      (display "bye ...")
-      (newline))))
+  (define (cadr  x) (car (cdr      x )))
+  (define (caddr x) (car (cdr (cdr x))))
+
+  (display "Welcome to MScheme")
+  (newline)
+  (display "press Ctrl-D to quit")
+  (newline)
+
+  (let ([restart '()])
+    (dynamic-wind
+      (lambda ()
+        (call-with-current-continuation
+          (lambda (c)
+            (set! restart c))))
+      (lambda ()
+        (REP (lambda () (display "--> "))))
+      (lambda ()
+        (let ([error (last-error)])
+          (if error
+            (let ([cause   (car   error)]
+                  [message (cadr  error)]
+                  [stack   (caddr error)])
+              (display "error     : ")
+              (display message)
+              (newline)
+              (display "caused by : ")
+              (display cause)
+              (newline)
+              (display stack)
+              (newline)
+              (restart '()))
+            (begin
+              (newline)
+              (display "bye ... ")
+              (newline))))))))
