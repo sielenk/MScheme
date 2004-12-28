671cb9b1d6a3d041e4198df7f6d644f83682109e
Marvin Sielenkemper <1593106+sielenk@users.noreply.github.com>
2004-12-28 10:25:41 +0000

    eclipse VisualSomething updates

--- MScheme/src/mscheme/AppletMain.java
+++ MScheme/src/mscheme/AppletMain.java
@@ -1,39 +1,53 @@
 /* Applet class for MScheme.
-   Copyright (C) 2001  Marvin H. Sielenkemper
+ Copyright (C) 2004 Marvin H. Sielenkemper
 
-This file is part of MScheme.
+ This file is part of MScheme.
 
-MScheme is free software; you can redistribute it and/or modify 
-it under the terms of the GNU General Public License as published by 
-the Free Software Foundation; either version 2 of the License, 
-or (at your option) any later version. 
+ MScheme is free software; you can redistribute it and/or modify 
+ it under the terms of the GNU General Public License as published by 
+ the Free Software Foundation; either version 2 of the License, 
+ or (at your option) any later version. 
 
-MScheme is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details. 
+ MScheme is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details. 
 
-You should have received a copy of the GNU General Public License
-along with MScheme; see the file COPYING. If not, write to 
-the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
-Boston, MA  02111-1307, USA. */
+ You should have received a copy of the GNU General Public License
+ along with MScheme; see the file COPYING. If not, write to 
+ the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ Boston, MA  02111-1307, USA. */
+
+/* Created on 28.12.2004, 09:50:48 by sielenk */
 
 package mscheme;
 
 import java.applet.Applet;
 
-public final class AppletMain
-    extends Applet
+/**
+ * @author sielenk
+ * 
+ * TODO To change the template for this generated type comment go to Window -
+ * Preferences - Java - Code Style - Code Templates
+ */
+public class AppletMain
+	extends Applet
 {
     public final static String CVS_ID
-        = "$Id$";
+    	= "$Id$";
 
 
     private StdioFrame frame = null;
 
+    public AppletMain()
+    {
+        super();
+    }
+
     public void init()
     {
         frame = new StdioFrame();
+        frame.setTitle("MScheme");
         frame.show();
     }
 
@@ -42,9 +56,9 @@ public final class AppletMain
         frame.hide();
         frame.dispose();
     }
-    
+
     public void start()
     {
         frame.start();
     }
-}
+} //  @jve:decl-index=0:visual-constraint="10,10"
--- MScheme/src/mscheme/StdioFrame.java
+++ MScheme/src/mscheme/StdioFrame.java
@@ -1,172 +1,207 @@
-/* Awt panel containing buttons and the io area.
-   Copyright (C) 2001  Marvin H. Sielenkemper
+/* TODO Add short description of this file here.
+ Copyright (C) 2004 Marvin H. Sielenkemper
 
-This file is part of MScheme.
+ This file is part of MScheme.
 
-MScheme is free software; you can redistribute it and/or modify 
-it under the terms of the GNU General Public License as published by 
-the Free Software Foundation; either version 2 of the License, 
-or (at your option) any later version. 
+ MScheme is free software; you can redistribute it and/or modify 
+ it under the terms of the GNU General Public License as published by 
+ the Free Software Foundation; either version 2 of the License, 
+ or (at your option) any later version. 
 
-MScheme is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details. 
+ MScheme is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details. 
 
-You should have received a copy of the GNU General Public License
-along with MScheme; see the file COPYING. If not, write to 
-the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
-Boston, MA  02111-1307, USA. */
+ You should have received a copy of the GNU General Public License
+ along with MScheme; see the file COPYING. If not, write to 
+ the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ Boston, MA  02111-1307, USA. */
+
+/* Created on 28.12.2004, 10:03:07 by sielenk */
 
 package mscheme;
 
-import java.awt.Button;
 import java.awt.Frame;
-import java.awt.Panel;
 
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.WindowEvent;
-import java.awt.event.WindowListener;
+import java.awt.Panel;
+import java.awt.Button;
 
 import mscheme.machine.Machine;
 
+/**
+ * @author sielenk
+ * 
+ * TODO To change the template for this generated type comment go to Window -
+ * Preferences - Java - Code Style - Code Templates
+ */
 public class StdioFrame
-    extends Frame
-    implements ActionListener, WindowListener, Runnable
+        extends Frame
+        implements Runnable
 {
-    public final static String CVS_ID
-        = "$Id$";
+    public final static String CVS_ID = "$Id$";
 
+    private StdioArea _stdio = null;
 
-    private StdioArea _stdio;
-    private Button    _cutB, _copyB, _pasteB;
+    private Panel _buttonPanel = null;
 
-    /***** interface ActionListener begin *****/
-    
-    public void actionPerformed(ActionEvent e) 
+    private Button _copyButton = null;
+
+    private Button _cutButton = null;
+
+    private Button _clearButton = null;
+
+    private Button _pasteButton = null;
+
+    public StdioFrame()
     {
-        String arg = e.getActionCommand();
-        
-        if (arg.equals("Clear"))
-        {
-            _stdio.clear();
-        }
-        else if (arg.equals(_copyB.getActionCommand()))
-        {
-            _stdio.copy();
-        }
-        else if (arg.equals(_cutB.getActionCommand())) 
-        {
-            _stdio.cut();
-        }
-        else if (arg.equals(_pasteB.getActionCommand()))
-        {
-            _stdio.paste();
-        }
-        else if (arg.equals(_stdio.getActionCommand()))
-        {
-            _pasteB.setEnabled(_stdio.canPaste());
-             _copyB.setEnabled(_stdio.canCopy ());
-              _cutB.setEnabled(_stdio.canCut  ());
-        }
-        
-        _stdio.requestFocus();
+        super();
+        initialize();
     }
 
-    /***** interface ActionListener end *****/
+    private void initialize()
+    {
+        this.setSize(600, 400);
+        this.setTitle("MScheme Frame");
+        this.add(get_buttonPanel(), java.awt.BorderLayout.SOUTH);
+        this.add(get_stdio(), java.awt.BorderLayout.CENTER);
+        this.addWindowListener(new java.awt.event.WindowAdapter()
+        {
+            public void windowOpened(java.awt.event.WindowEvent e)
+            {
+                _runner.start();
+                get_stdio().requestFocus();
+            }
 
-    /***** interface Runnable begin *****/
+            public void windowClosing(java.awt.event.WindowEvent e)
+            {
+                _runner.interrupt();
+                dispose();
+            }
+        });
+    }
+
+    private Panel get_buttonPanel()
+    {
+        if (_buttonPanel == null)
+        {
+            _buttonPanel = new Panel();
+            _buttonPanel.add(get_clearButton(), null);
+            _buttonPanel.add(get_copyButton(), null);
+            _buttonPanel.add(get_cutButton(), null);
+            _buttonPanel.add(get_pasteButton(), null);
+        }
+        return _buttonPanel;
+    }
+
+    private Button get_copyButton()
+    {
+        if (_copyButton == null)
+        {
+            _copyButton = new Button();
+            _copyButton.setLabel("Copy");
+            _copyButton.addActionListener(new java.awt.event.ActionListener()
+            {
+                public void actionPerformed(java.awt.event.ActionEvent e)
+                {
+                    get_stdio().copy();
+                    get_stdio().requestFocus();
+                }
+            });
+        }
+        return _copyButton;
+    }
+
+    private Button get_cutButton()
+    {
+        if (_cutButton == null)
+        {
+            _cutButton = new Button();
+            _cutButton.setLabel("Cut");
+            _cutButton.addActionListener(new java.awt.event.ActionListener()
+            {
+                public void actionPerformed(java.awt.event.ActionEvent e)
+                {
+                    get_stdio().cut();
+                    get_stdio().requestFocus();
+                }
+            });
+        }
+        return _cutButton;
+    }
+
+    private Button get_clearButton()
+    {
+        if (_clearButton == null)
+        {
+            _clearButton = new Button();
+            _clearButton.setLabel("Clear");
+            _clearButton.addActionListener(new java.awt.event.ActionListener()
+            {
+                public void actionPerformed(java.awt.event.ActionEvent e)
+                {
+                    get_stdio().clear();
+                    get_stdio().requestFocus();
+                }
+            });
+        }
+        return _clearButton;
+    }
+
+    private Button get_pasteButton()
+    {
+        if (_pasteButton == null)
+        {
+            _pasteButton = new Button();
+            _pasteButton.setLabel("Paste");
+            _pasteButton.addActionListener(new java.awt.event.ActionListener()
+            {
+                public void actionPerformed(java.awt.event.ActionEvent e)
+                {
+                    get_stdio().paste();
+                    get_stdio().requestFocus();
+                }
+            });
+        }
+        return _pasteButton;
+    }
+
+    private StdioArea get_stdio()
+    {
+        if (_stdio == null)
+        {
+            _stdio = new StdioArea();
+            _stdio.addActionListener(new java.awt.event.ActionListener()
+            {
+                public void actionPerformed(java.awt.event.ActionEvent e)
+                {
+                    get_pasteButton().setEnabled(get_stdio().canPaste());
+                    get_copyButton().setEnabled(get_stdio().canCopy());
+                    get_cutButton().setEnabled(get_stdio().canCut());
+                    get_stdio().requestFocus();
+                }
+            });
+        }
+        return _stdio;
+    }
+
+    /** *** interface Runnable begin **** */
+
+    private final Thread _runner = new Thread(this);
 
     public void run()
     {
-        new Machine(
-            _stdio.stdin (),
-            _stdio.stdout()
-        ).run();
+        new Machine(get_stdio().stdin(), get_stdio().stdout()).run();
         dispose();
     }
 
-    /***** interface Runnable end *****/
-
-    /***** interface WindowListener begin *****/
-
-    private final Thread runner = new Thread(this);
+    /** *** interface Runnable end **** */
 
     public void start()
     {
-        if (!runner.isAlive())
+        if (!_runner.isAlive())
         {
-            runner.start();
+            _runner.start();
         }
     }
-
-    public void windowOpened(WindowEvent e)
-    {
-        runner.start();
-        _stdio.requestFocus();
-    }
-
-    public void windowClosing(WindowEvent e)
-    {
-        runner.interrupt();
-        dispose();
-    }
-
-    public void windowClosed(WindowEvent e)
-    { }
-
-    public void windowActivated(WindowEvent e)
-    { }
-
-    public void windowDeactivated(WindowEvent e)
-    { }
-
-    public void windowDeiconified(WindowEvent e)
-    { }
-
-    public void windowIconified(WindowEvent e)
-    { }
-
-    /***** interface WindowListener end *****/
-    
-    /***** frame initialisation function begin *****/
-     
-    public StdioFrame()
-    {
-        super("MScheme");
-        setSize(600, 400);
-
-        {
-            Panel p = new Panel();
-
-            Button clearB = new Button("Clear");
-            clearB.addActionListener(this); 
-            p.add(clearB);
-
-            _copyB = new Button("Copy");
-            _copyB.addActionListener(this);
-            p.add(_copyB);
-
-            _cutB = new Button("Cut");
-            _cutB.addActionListener(this);
-            p.add(_cutB);
-
-            _pasteB = new Button("Paste");
-            _pasteB.addActionListener(this);
-            p.add(_pasteB);
-
-            add(p, "South");
-        }
-
-        {
-            _stdio = new StdioArea();
-            _stdio.addActionListener(this);
-            add(_stdio);
-        }
-
-        addWindowListener(this);
-    }
-
-    /***** frame initialisation function end *****/
 }
