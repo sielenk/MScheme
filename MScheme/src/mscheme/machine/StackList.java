0c7e6bb97b11c7a6f88963714bc7cf9d0a51020e
Marvin Sielenkemper <1593106+sielenk@users.noreply.github.com>
2005-08-05 19:05:04 +0000

    internal logic rework

--- MScheme/src/mscheme/machine/StackList.java
+++ MScheme/src/mscheme/machine/StackList.java
@@ -1,38 +1,62 @@
-/*
- * Created on 02.01.2004
- *
- * To change the template for this generated file go to
- * Window&gt;Preferences&gt;Java&gt;Code Generation&gt;Code and Comments
- */
+/* StackList class for MScheme.
+   Copyright (C) 2004  Marvin H. Sielenkemper
+
+This file is part of MScheme.
+
+MScheme is free software; you can redistribute it and/or modify 
+it under the terms of the GNU General Public License as published by 
+the Free Software Foundation; either version 2 of the License, 
+or (at your option) any later version. 
+
+MScheme is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details. 
+
+You should have received a copy of the GNU General Public License
+along with MScheme; see the file COPYING. If not, write to 
+the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+Boston, MA  02111-1307, USA. */
+
 package mscheme.machine;
 
 import mscheme.exceptions.RuntimeError;
 
-/**
- * @author sielenk
+
+/** 
+ * Class invariants:
+ * <ul>
+ * <li><code>_head != null</code></li>
+ * </ul>
+ *
+ * @author Marvin H. Sielenkemper
  */
 class StackList implements IStack
 {
-	private final Slice _root;
 	private final Mark  _call_cc_mark;
 	private Slice       _head;
 
 	StackList()
 	{
-		_root         = new Slice();
-		_head         = new Slice(_root);
-		_call_cc_mark = _head._mark;
+        _call_cc_mark = new Mark();
+		_head         = new Slice(_call_cc_mark, null);
 	}
 
 	public boolean isEmpty()
 	{
-		return getStack().isEmpty();
+        Slice slice = _head;
+        while (slice._next != null && slice._stack.isEmpty())
+            slice = slice._next;
+
+        return slice._stack.isEmpty();
 	}
 
 	public StackFrame pop()
 	{
-		_head = getTosEntry();
-		return _head._stack.pop();
+        while (_head._next != null && _head._stack.isEmpty())
+            _head = _head._next;
+
+        return _head._stack.pop();
 	}
 
 	public void push(StackFrame f)
@@ -40,46 +64,37 @@ class StackList implements IStack
 		_head._stack.push(f);
 	}
 
-	private Slice getTosEntry()
-	{
-        Slice e = _head;
-		
-		while (e._stack.isEmpty() && (e._next != _root))
-		{
-			e = e._next;
-		}
-
-		return e;
-	}
-
-	private IStack getStack()
-	{
-		return getTosEntry()._stack;
-	}
-
 
 	public static class Mark
 	{
-		public Slice cutSlice(StackList l) throws RuntimeError
+		public Slice cutSlice(final StackList l) throws RuntimeError
 		{
-			final Slice leaf = l._head;
-                  Slice root = leaf;
+            final Slice head = l._head;
 
-			while ((root._mark != this)  && (root._next != null))
-			{
-				root = root._next;
+            if (this == l._call_cc_mark)
+            {
+                // no search neccessary:
+                // this mark always denotes the last Slice
+
+                l._head = new Slice(l._call_cc_mark, null);
+                return head;
+            }
+
+            // scan for a matching slice
+            // leave the loop one before the last slice
+            
+            for (Slice slice = head; slice._next != null; slice = slice._next)
+            {
+                if (slice._mark == this)
+                {
+                    l._head = slice._next;
+                    slice._next = null;
+
+                    return head;
+                }
 			}
 
-			if (root._next != null)
-			{
-				l._head = root._next;
-				root._next= null;
-				return leaf;
-			}
-			else
-			{
-				throw new RuntimeError(null, "");
-			}
+            throw new RuntimeError(null, "");
 		}
 	}
 
@@ -88,31 +103,22 @@ class StackList implements IStack
 		public final static String CVS_ID
 		   = "$Id$";
 
-        Slice            _next;
         final Mark       _mark;
 		final StackPlain _stack;
+        Slice            _next;
 
-        Slice()
-		{
-			_next  = null;
-			_mark  = null;
-			_stack = new StackPlain();
-		}
+        Slice(Mark mark, Slice next)
+        {
+            _mark  = mark;
+            _stack = new StackPlain();
+            _next  = next;
+        }
 
-        Slice(Slice next)
+        Slice(Slice slice, Slice next)
 		{
-			_next  = next;
-			_mark  = new Mark();
-			_stack = new StackPlain();
-		}
-
-        Slice(Slice leaf, Slice base)
-		{
-			_next  = (leaf._next == null)
-			         ? base
-			         : new Slice(leaf._next, base);
-			_mark  = leaf._mark;
-			_stack = leaf._stack.getCopy();
+            _mark  = slice._mark;
+            _stack = slice._stack.getCopy();
+            _next  = next;
 		}
 	}
 
@@ -125,12 +131,20 @@ class StackList implements IStack
 
 	public Mark createMark()
 	{
-		_head = new Slice(_head);
+        _head = new Slice(new Mark(), _head);
 		return _head._mark;
 	}
 
-	public void reinstate(Slice slice)
+	public void reinstate(final Slice slice)
 	{
-		_head = new Slice(slice, _head);
+        final Slice oldHead = _head;
+
+        Slice srcSlice = slice;
+        Slice dstSlice = _head = new Slice(srcSlice, oldHead);
+        while (srcSlice._next != null)
+        {
+            srcSlice = srcSlice._next;
+            dstSlice = dstSlice._next = new Slice(srcSlice, oldHead);
+        }
 	}
 }
