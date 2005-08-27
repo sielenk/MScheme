60442321f9a70d92b543e0dc639fe3f7233c58c0
sielenk <sielenk@14e62dca-297b-4487-9397-64a3d55ac3ab>
2005-08-27 12:30:55 +0000

    merged Continuation and Subcontinuation

--- trunk/MScheme/src/mscheme/values/functions/CallCCFunction.java
+++ trunk/MScheme/src/mscheme/values/functions/CallCCFunction.java
@@ -43,7 +43,7 @@ public final class CallCCFunction
                 state,
                 argument,
                 ListFactory.create(
-                        new Continuation(
+                        new Subcontinuation(
                                 state)));
     }
 }
