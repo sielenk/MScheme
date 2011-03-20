8dbe34d894cb754587c19235f1996ae010598f2d
sielenk <sielenk@14e62dca-297b-4487-9397-64a3d55ac3ab>
2011-03-20 08:48:50 +0000

    Updates due to Eclipse updates.

--- trunk/MScheme/src/mscheme/values/functions/Builtins.java
+++ trunk/MScheme/src/mscheme/values/functions/Builtins.java
@@ -950,7 +950,7 @@ public class Builtins
         return buf.toString();
     }
 
-    private static boolean paramsValid(Class[] params)
+    private static boolean paramsValid(Class<?>[] params)
     {
         if (params.length == 1)
         {
