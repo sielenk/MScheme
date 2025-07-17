82d0b3fa986f2fdd3fa41824ef187830eda16a16
Marvin Sielenkemper <sielenk@gmx.de>
2025-07-17 16:33:16 +0200

    Migrate `PlainStack.java` to Kotlin for improved readability and modern language features. Rename .java to .kt

--- MScheme/src/main/java/mscheme/machine/stack/PlainStack.kt
+++ MScheme/src/main/java/mscheme/machine/stack/PlainStack.kt
@@ -4,69 +4,67 @@
  * To change the template for this generated file go to
  * Window&gt;Preferences&gt;Java&gt;Code Generation&gt;Code and Comments
  */
-package mscheme.machine.stack;
+package mscheme.machine.stack
 
-import mscheme.machine.StackFrame;
-
+import mscheme.machine.StackFrame
 
 /**
  * @author sielenk
  */
-class PlainStack implements IStack {
+internal class PlainStack : IStack {
+    private var _stack: Array<StackFrame?>
+    private var _sp: Int
 
-  private final static int INITIAL_STACK_SIZE = 0;
+    constructor(other: PlainStack) {
+        _sp = other._sp
+        _stack = arrayOfNulls<StackFrame>(_sp)
 
-  private StackFrame[] _stack;
-  private int _sp;
-
-
-  public PlainStack(PlainStack other) {
-    _sp = other._sp;
-    _stack = new StackFrame[_sp];
-
-    System.arraycopy(other._stack, 0, _stack, 0, _sp);
-  }
-
-  public PlainStack() {
-    _sp = 0;
-    _stack = new StackFrame[INITIAL_STACK_SIZE];
-  }
-
-  public PlainStack getCopy() {
-    return new PlainStack(this);
-  }
-
-  public boolean isEmpty() {
-    return _sp <= 0;
-  }
-
-  public StackFrame pop() {
-    assertFull();
-    return _stack[--_sp];
-  }
-
-  public void push(StackFrame frame) {
-    if (_sp >= _stack.length) {
-      enlarge();
+        System.arraycopy(other._stack, 0, _stack, 0, _sp)
     }
 
-    _stack[_sp++] = frame;
-  }
-
-
-  private void assertFull() {
-    if (isEmpty()) {
-      throw new ArrayIndexOutOfBoundsException();
+    constructor() {
+        _sp = 0
+        _stack = arrayOfNulls<StackFrame>(INITIAL_STACK_SIZE)
     }
-  }
 
-  private void enlarge() {
-    final int length = _stack.length;
-    final StackFrame[] oldStack = _stack;
-    final StackFrame[] newStack = new StackFrame[length * 2 + 1];
+    val copy: PlainStack
+        get() = PlainStack(this)
 
-    System.arraycopy(oldStack, 0, newStack, 0, length);
+    override fun isEmpty(): Boolean {
+        return _sp <= 0
+    }
 
-    _stack = newStack;
-  }
+    override fun pop(): StackFrame? {
+        assertFull()
+        return _stack[--_sp]
+    }
+
+    override fun push(frame: StackFrame?) {
+        if (_sp >= _stack.size) {
+            enlarge()
+        }
+
+        _stack[_sp++] = frame
+    }
+
+
+    private fun assertFull() {
+        if (isEmpty()) {
+            throw ArrayIndexOutOfBoundsException()
+        }
+    }
+
+    private fun enlarge() {
+        val length = _stack.size
+        val oldStack = _stack
+        val newStack = arrayOfNulls<StackFrame>(length * 2 + 1)
+
+        System.arraycopy(oldStack, 0, newStack, 0, length)
+
+        _stack = newStack
+    }
+
+    companion object {
+        private const val INITIAL_STACK_SIZE = 0
+    }
 }
