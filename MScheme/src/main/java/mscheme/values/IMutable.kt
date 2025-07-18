9343d87dda2896e68bc2bc984f5ef6fedee54b53
Marvin Sielenkemper <sielenk@gmx.de>
2025-07-18 15:44:31 +0200

    Migrate `ScmVector.java` to Kotlin for consistency and improved readability. Rename .java to .kt.

--- MScheme/src/main/java/mscheme/values/IMutable.kt
+++ MScheme/src/main/java/mscheme/values/IMutable.kt
@@ -8,10 +8,7 @@ package mscheme.values
 
 /**
  * @author sielenk
- *
- * To change the template for this generated type comment go to
- * Window&gt;Preferences&gt;Java&gt;Code Generation&gt;Code and Comments
  */
 interface IMutable {
-    fun getConst(): Any
+    fun getConst(): Any?
 }
