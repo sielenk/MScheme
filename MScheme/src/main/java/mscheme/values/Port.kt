31b203b217d30c4d0549cd988d98c9d9d8c95d59
Marvin Sielenkemper <sielenk@gmx.de>
2025-07-18 15:46:16 +0200

    Migrate `Port.java` to Kotlin for consistency and improved readability. Rename .java to .kt.

--- MScheme/src/main/java/mscheme/values/Port.kt
+++ MScheme/src/main/java/mscheme/values/Port.kt
@@ -17,11 +17,6 @@ You should have received a copy of the GNU General Public License
 along with MScheme; see the file COPYING. If not, write to 
 the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
 Boston, MA  02111-1307, USA. */
+package mscheme.values
 
-package mscheme.values;
-
-public abstract class Port {
-
-  protected Port() {
-  }
-}
+abstract class Port protected constructor()
