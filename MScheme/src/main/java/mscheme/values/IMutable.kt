e32531bc7c357d475fe9747fc405081ae14fbe17
Marvin Sielenkemper <sielenk@gmx.de>
2025-07-18 14:07:25 +0200

    Migrate `IMutable.java` to Kotlin for improved readability and modern language features. Rename .java to .kt

--- MScheme/src/main/java/mscheme/values/IMutable.kt
+++ MScheme/src/main/java/mscheme/values/IMutable.kt
@@ -4,7 +4,7 @@
  * To change the template for this generated file go to
  * Window&gt;Preferences&gt;Java&gt;Code Generation&gt;Code and Comments
  */
-package mscheme.values;
+package mscheme.values
 
 /**
  * @author sielenk
@@ -12,7 +12,6 @@ package mscheme.values;
  * To change the template for this generated type comment go to
  * Window&gt;Preferences&gt;Java&gt;Code Generation&gt;Code and Comments
  */
-public interface IMutable {
-
-  Object getConst();
+interface IMutable {
+    fun getConst(): Any
 }
