a319abd7cdf327be577c44afa7db1e2cd05e5c2b
Marvin Sielenkemper <sielenk@gmx.de>
2025-07-22 12:04:50 +0200

    Migrate `CompiledLambda.java` to Kotlin for consistency and improved readability. Rename .java to .kt.

--- MScheme/src/main/java/mscheme/code/CompiledLambda.kt
+++ MScheme/src/main/java/mscheme/code/CompiledLambda.kt
@@ -17,93 +17,75 @@
  * MScheme; see the file COPYING. If not, write to the Free Software Foundation,
  * Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
  */
+package mscheme.code
 
-package mscheme.code;
+import mscheme.code.Sequence.Companion.create
+import mscheme.compiler.Compiler.Companion.force
+import mscheme.compiler.IForceable
+import mscheme.environment.DynamicEnvironment
+import mscheme.environment.StaticEnvironment
+import mscheme.exceptions.CompileError
+import mscheme.exceptions.ListExpected
+import mscheme.exceptions.PairExpected
+import mscheme.exceptions.SchemeException
+import mscheme.machine.Registers
+import mscheme.util.Arity
+import mscheme.values.IList
+import mscheme.values.functions.CheckedFunction
+import java.io.IOException
+import java.io.Writer
 
-import java.io.IOException;
-import java.io.Writer;
-import mscheme.compiler.Compiler;
-import mscheme.compiler.IForceable;
-import mscheme.environment.DynamicEnvironment;
-import mscheme.environment.StaticEnvironment;
-import mscheme.exceptions.CompileError;
-import mscheme.exceptions.ListExpected;
-import mscheme.exceptions.PairExpected;
-import mscheme.exceptions.SchemeException;
-import mscheme.machine.Registers;
-import mscheme.util.Arity;
-import mscheme.values.IList;
-import mscheme.values.functions.CheckedFunction;
-import org.jetbrains.annotations.NotNull;
+class CompiledLambda private constructor(
+    private val _arity: Arity,
+    private val _frameSize: Int,
+    private var _compiledBody: Any?
+) : IForceable, IReduceable {
+    internal inner class Closure(
+        private val _enclosingEnvironment: DynamicEnvironment
+    ) : CheckedFunction() {
+        @Throws(IOException::class)
+        fun write(destination: Writer) {
+            destination.write("#[closure]")
+        }
 
-public final class CompiledLambda
-    implements IForceable, IReduceable {
+        override val arity: Arity
+            get() = _arity
 
-  private final Arity _arity;
+        @Throws(ListExpected::class, PairExpected::class)
+        override fun checkedCall(state: Registers, args: IList): Any? {
+            val newEnvironment = _enclosingEnvironment.createChild(
+                _arity, _frameSize, args
+            )
 
-  private final int _frameSize;
+            state.environment = newEnvironment
 
-  private Object _compiledBody;
-
-  private CompiledLambda(Arity arity, int frameSize, Object compiledBody) {
-    _arity = arity;
-    _frameSize = frameSize;
-    _compiledBody = compiledBody;
-  }
-
-  public static CompiledLambda create(Arity arity, int frameSize,
-      Object compiledBody) {
-    return new CompiledLambda(arity, frameSize, compiledBody);
-  }
-
-  public static CompiledLambda create(Arity arity, IList body,
-      StaticEnvironment env)
-      throws SchemeException, InterruptedException {
-    Object compiledBody = Sequence.create(body.getCompiledArray(env));
-
-    return create(arity, env.getSize(), compiledBody);
-  }
-
-  final class Closure
-      extends CheckedFunction {
-
-    private final DynamicEnvironment _enclosingEnvironment;
-
-    public Closure(DynamicEnvironment enclosingEnvironment) {
-      _enclosingEnvironment = enclosingEnvironment;
+            return _compiledBody
+        }
     }
 
-    public void write(Writer destination)
-        throws IOException {
-      destination.write("#[closure]");
+    @Throws(CompileError::class)
+    override fun force(): Any? {
+        _compiledBody = force(_compiledBody)
+        return this
     }
 
-    public Arity getArity() {
-      return _arity;
+    override fun toString(): String =
+        "lambda:<$_compiledBody>"
+
+    override fun reduce(state: Registers): Any =
+        Closure(state.environment)
+
+    companion object {
+        @JvmStatic
+        fun create(arity: Arity, frameSize: Int, compiledBody: Any?): CompiledLambda =
+            CompiledLambda(arity, frameSize, compiledBody)
+
+        @JvmStatic
+        @Throws(SchemeException::class, InterruptedException::class)
+        fun create(arity: Arity, body: IList, env: StaticEnvironment): CompiledLambda {
+            val compiledBody = create(body.getCompiledArray(env))
+
+            return create(arity, env.size, compiledBody)
+        }
     }
-
-    protected Object checkedCall(Registers state, IList arguments)
-        throws ListExpected, PairExpected {
-      DynamicEnvironment newEnvironment = _enclosingEnvironment
-          .createChild(_arity, _frameSize, arguments);
-
-      state.setEnvironment(newEnvironment);
-
-      return _compiledBody;
-    }
-  }
-
-  public Object force()
-      throws CompileError {
-    _compiledBody = Compiler.force(_compiledBody);
-    return this;
-  }
-
-  public String toString() {
-    return "lambda:<" + _compiledBody + '>';
-  }
-
-  public Object reduce(@NotNull Registers state) {
-    return new Closure(state.getEnvironment());
-  }
 }
